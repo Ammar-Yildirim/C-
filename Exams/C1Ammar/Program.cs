936f78412fcf5fbd34089d10fd9f11fb753c4905
Ammar Yildirim <65595544+Ammar-Yildirim@users.noreply.github.com>
2023-02-03 08:55:05 +0100

    Add files via upload
    Exams during the first semester, in the subject of Programming

---
+++ Exams/C1Ammar/Program.cs
@@ -0,0 +1,117 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Xml.Linq;
+
+namespace C1Ammar
+{
+    struct Array
+    {
+        public string name;
+        public int count;
+    }
+    internal class Program
+    {
+        static void readData(out int N, out Array[] candies)
+        {
+            string tmp;
+            string tmps;
+            bool error, error40;
+            error40 = true;
+
+            do
+            {
+                Console.Write("N: ");
+                tmp = Console.ReadLine();
+                error = (!Int32.TryParse(tmp, out N) || (N<1));
+                if (error)
+                {
+                    Console.Error.WriteLine("ERROR!Count can not be 0");
+                }
+            } while (error);
+
+            candies = new Array[N + 1];
+            do
+            {
+                for (int i = 1; i <= N; i++)
+                {
+                    Console.WriteLine("Enter Data " + i);
+                    Console.WriteLine("Enter name: ");
+                    candies[i].name = Console.ReadLine();
+                    do
+                    {
+                        Console.WriteLine("Enter count");
+                        tmps = Console.ReadLine();
+                        error = (!Int32.TryParse(tmps, out candies[i].count));
+                        if (error)
+                        {
+                            Console.Error.WriteLine("ERROR!Please put an integer as the count of candies");
+                        }
+                        if(candies[i].count > 40)
+                        {
+                            error40 = false;
+                        }
+                    } while (error);
+                }
+                if (error40)
+                {
+                    Console.Error.WriteLine("ERROR!At least one candy should have more than 40, Start over");
+                }
+            } while (error40);
+        }
+        static void task1(int N, Array[] candies, out string name)
+        {
+            int i = 1;
+            while (candies[i].count <= 40)
+            {
+                i++;
+            }
+            name = candies[i].name;
+        }
+        static void task2(int N, Array[] candies, out float remainedPercent)
+        {
+            int s = 0;
+            int s_t = 0;
+
+            for (int i = 1; i <= N; i++)
+            {
+                s = s + candies[i].count;
+                if (candies[i].name != "teasers")
+                {
+                    s_t = s_t + candies[i].count;
+                }
+            }
+            remainedPercent = 100 * s_t / s;
+        }
+
+        static void writeData(string name, float remainedPercent)
+        {
+            Console.WriteLine(name + " has more than 40");
+            Console.WriteLine(remainedPercent + "% remain");
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("C1 Coding Test : Ammar Yildirim FA27QM");
+            ///Declaration
+            ///
+            int N;
+            Array[] candies;
+
+            string name;
+            float remainedPercent;
+
+            ///Input
+            ///
+            readData(out N, out candies);
+
+            ///Implementation
+            ///
+            task1(N, candies, out name);
+            task2(N, candies, out remainedPercent);
+
+            ///Output
+            ///
+            writeData(name, remainedPercent);
+
+        }
+    }
+}
---
+++ Exams/C2Exam/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+
+namespace C2Exam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            <YILDIRIM AMMAR>, <FA27QM>
+            This solution was prepared and submitted by the student stated above for the
+            assignment of the Programming course. I declare that this solution is my own
+            work. I have not copied or used third party solutions. I have not passed my
+            solution to my classmates, neither made it public.
+            Students’ regulation of Eötvös Loránd University (ELTE Regulations Vol. II.
+            74/C.§) states that as long as a student presents another student’s work -
+            or at least the significant part of it - as his/her own performance, it will
+            count as a disciplinary fault. The most serious consequence of a disciplinary
+            fault can be dismissal of the student from the University.
+            */
+            ///Declaration
+            ///
+            const int MaxN = 1000;
+            const int MaxR = 1000;
+
+            int N;
+            int M;
+
+            int[,] miles = new int[MaxN + 1, MaxR + 1];
+            int cnt40 = 0;
+            int[] distance = new int[MaxN + 1];
+            int biggest = 0;
+
+            int cntMoreThanM = 0;
+            int[] moreThanM = new int[MaxN + 1];
+
+            int[] shorts = new int[MaxN + 1];
+            bool lessThanShortest = false;
+
+            ///Input
+            ///
+
+            string[] tmp = Console.ReadLine().Split(" ");
+            N = Int32.Parse(tmp[0]);
+            M = Int32.Parse(tmp[1]);
+
+            for (int i = 0; i < N; i++)
+            {
+                string[] tmps = Console.ReadLine().Split();
+                int R = Int32.Parse(tmps[0]);
+                shorts[i] = 9999999;
+                for (int j = 0; j < R; j++)
+                {
+                    miles[i, j] = Int32.Parse(tmps[j + 1]);
+
+                    //Solution for task 1
+                    if (miles[i, j] == 40)
+                    {
+                        cnt40++;
+                    }
+                    //
+                    distance[i] += miles[i, j];
+                    //
+                    if (miles[i, j] < shorts[i])
+                    {
+                        shorts[i] = miles[i, j];
+                    }
+
+                }
+            }
+
+
+            ///Algorithm
+            ///
+
+            //Solve task 2
+            for (int i = 1; i < N; i++)
+            {
+                if (distance[i] > distance[biggest])
+                {
+                    biggest = i;
+                }
+            }
+            //Solve task 3
+            for (int i = 0; i < N; i++)
+            {
+                if (distance[i] > M)
+                {
+                    moreThanM[cntMoreThanM++] = i + 1;
+                }
+            }
+            //Solve task 4
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    if (shorts[i] > distance[j])
+                    {
+                        lessThanShortest = true;
+                    }
+                }
+            }
+
+            ///Output
+            ///
+            Console.WriteLine(cnt40);
+            Console.WriteLine(biggest + 1);
+            Console.Write(cntMoreThanM);
+            for (int i = 0; i < cntMoreThanM; i++)
+            {
+                Console.Write(" " + moreThanM[i]);
+            }
+            Console.WriteLine();
+            if (lessThanShortest == true)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
